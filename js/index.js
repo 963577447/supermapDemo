c08d0fdd30366b3e1c8a4cb8c5a328de7c184f7d
guiling <963577447@qq.com>
2018-11-15 00:01:06 +0800

    测试

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     let map, tiandituLayer,tianMarkerLayer,markerlayer,marker,drawPointLayer,vector,visualAngleVector;
     let drawPoint,drawLine;
     init();
-
+    console.log("ssssss");
     function init() {
         if (!document.createElement('canvas').getContext) {
             sweetAlert(
